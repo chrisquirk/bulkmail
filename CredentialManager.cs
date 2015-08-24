84b373d77c3163befd9cecbc54d8980f6dc414d3
Chris Quirk <chrisq@microsoft.com>
2015-08-23 19:50:47 -0700

    initial commit

---
+++ CredentialManager.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+using System.Net;
+
+public static class CredentialManager
+{
+    public static Credential ReadCredential(string applicationName)
+    {
+        IntPtr nCredPtr;
+        bool read = CredRead(applicationName, CredentialType.Generic, 0, out nCredPtr);
+        if (read)
+        {
+            using (CriticalCredentialHandle critCred = new CriticalCredentialHandle(nCredPtr))
+            {
+                CREDENTIAL cred = critCred.GetCredential();
+                return ReadCredential(cred);
+            }
+        }
+
+        return null;
+    }
+
+    private static Credential ReadCredential(CREDENTIAL credential)
+    {
+        string applicationName = Marshal.PtrToStringUni(credential.TargetName);
+        string userName = Marshal.PtrToStringUni(credential.UserName);
+        string secret = null;
+        if (credential.CredentialBlob != IntPtr.Zero)
+        {
+            secret = Marshal.PtrToStringUni(credential.CredentialBlob, (int)credential.CredentialBlobSize / 2);
+        }
+
+        return new Credential(credential.Type, applicationName, userName, secret);
+    }
+
+    public static void WriteCredential(string applicationName, string userName, string secret)
+    {
+        byte[] byteArray = secret == null ? null : Encoding.Unicode.GetBytes(secret);
+        // XP and Vista: 512; 
+        // 7 and above: 5*512
+        if (Environment.OSVersion.Version < new Version(6, 1) /* Windows 7 */)
+        {
+            if (byteArray != null && byteArray.Length > 512)
+                throw new ArgumentOutOfRangeException("secret", "The secret message has exceeded 512 bytes.");
+        }
+        else
+        {
+            if (byteArray != null && byteArray.Length > 512 * 5)
+                throw new ArgumentOutOfRangeException("secret", "The secret message has exceeded 2560 bytes.");
+        }
+
+        CREDENTIAL credential = new CREDENTIAL();
+        credential.AttributeCount = 0;
+        credential.Attributes = IntPtr.Zero;
+        credential.Comment = IntPtr.Zero;
+        credential.TargetAlias = IntPtr.Zero;
+        credential.Type = CredentialType.Generic;
+        credential.Persist = (uint)CredentialPersistence.LocalMachine;
+        credential.CredentialBlobSize = (uint)(byteArray == null ? 0 : byteArray.Length);
+        credential.TargetName = Marshal.StringToCoTaskMemUni(applicationName);
+        credential.CredentialBlob = Marshal.StringToCoTaskMemUni(secret);
+        credential.UserName = Marshal.StringToCoTaskMemUni(userName ?? Environment.UserName);
+
+        bool written = CredWrite(ref credential, 0);
+        Marshal.FreeCoTaskMem(credential.TargetName);
+        Marshal.FreeCoTaskMem(credential.CredentialBlob);
+        Marshal.FreeCoTaskMem(credential.UserName);
+
+        if (!written)
+        {
+            int lastError = Marshal.GetLastWin32Error();
+            throw new Exception(string.Format("CredWrite failed with the error code {0}.", lastError));
+        }
+    }
+
+    public static IReadOnlyList<Credential> EnumerateCrendentials()
+    {
+        List<Credential> result = new List<Credential>();
+
+        int count;
+        IntPtr pCredentials;
+        bool ret = CredEnumerate(null, 0, out count, out pCredentials);
+        if (ret)
+        {
+            for (int n = 0; n < count; n++)
+            {
+                IntPtr credential = Marshal.ReadIntPtr(pCredentials, n * Marshal.SizeOf(typeof(IntPtr)));
+                result.Add(ReadCredential((CREDENTIAL)Marshal.PtrToStructure(credential, typeof(CREDENTIAL))));
+            }
+        }
+        else
+        {
+            int lastError = Marshal.GetLastWin32Error();
+            throw new Win32Exception(lastError);
+        }
+
+        return result;
+    }
+
+    [DllImport("Advapi32.dll", EntryPoint = "CredReadW", CharSet = CharSet.Unicode, SetLastError = true)]
+    static extern bool CredRead(string target, CredentialType type, int reservedFlag, out IntPtr credentialPtr);
+
+    [DllImport("Advapi32.dll", EntryPoint = "CredWriteW", CharSet = CharSet.Unicode, SetLastError = true)]
+    static extern bool CredWrite([In] ref CREDENTIAL userCredential, [In] UInt32 flags);
+
+    [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+    static extern bool CredEnumerate(string filter, int flag, out int count, out IntPtr pCredentials);
+
+    [DllImport("Advapi32.dll", EntryPoint = "CredFree", SetLastError = true)]
+    static extern bool CredFree([In] IntPtr cred);
+
+    private enum CredentialPersistence : uint
+    {
+        Session = 1,
+        LocalMachine,
+        Enterprise
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    private struct CREDENTIAL
+    {
+        public uint Flags;
+        public CredentialType Type;
+        public IntPtr TargetName;
+        public IntPtr Comment;
+        public System.Runtime.InteropServices.ComTypes.FILETIME LastWritten;
+        public uint CredentialBlobSize;
+        public IntPtr CredentialBlob;
+        public uint Persist;
+        public uint AttributeCount;
+        public IntPtr Attributes;
+        public IntPtr TargetAlias;
+        public IntPtr UserName;
+    }
+
+    sealed class CriticalCredentialHandle : CriticalHandleZeroOrMinusOneIsInvalid
+    {
+        public CriticalCredentialHandle(IntPtr preexistingHandle)
+        {
+            SetHandle(preexistingHandle);
+        }
+
+        public CREDENTIAL GetCredential()
+        {
+            if (!IsInvalid)
+            {
+                CREDENTIAL credential = (CREDENTIAL)Marshal.PtrToStructure(handle, typeof(CREDENTIAL));
+                return credential;
+            }
+
+            throw new InvalidOperationException("Invalid CriticalHandle!");
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            if (!IsInvalid)
+            {
+                CredFree(handle);
+                SetHandleAsInvalid();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
+public enum CredentialType
+{
+    Generic = 1,
+    DomainPassword,
+    DomainCertificate,
+    DomainVisiblePassword,
+    GenericCertificate,
+    DomainExtended,
+    Maximum,
+    MaximumEx = Maximum + 1000,
+}
+
+public class Credential
+{
+    private readonly string _applicationName;
+    private readonly string _userName;
+    private readonly string _password;
+    private readonly CredentialType _credentialType;
+
+    public CredentialType CredentialType
+    {
+        get { return _credentialType; }
+    }
+
+    public string ApplicationName
+    {
+        get { return _applicationName; }
+    }
+
+    public string UserName
+    {
+        get { return _userName; }
+    }
+
+    public string Password
+    {
+        get { return _password; }
+    }
+
+    public Credential(CredentialType credentialType, string applicationName, string userName, string password)
+    {
+        _applicationName = applicationName;
+        _userName = userName;
+        _password = password;
+        _credentialType = credentialType;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("CredentialType: {0}, ApplicationName: {1}, UserName: {2}, Password: {3}", CredentialType, ApplicationName, UserName, Password);
+    }
+
+
+    [DllImport("ole32.dll")]
+    public static extern void CoTaskMemFree(IntPtr ptr);
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    private struct CREDUI_INFO
+    {
+        public int cbSize;
+        public IntPtr hwndParent;
+        public string pszMessageText;
+        public string pszCaptionText;
+        public IntPtr hbmBanner;
+    }  
+
+
+    [DllImport("credui.dll", CharSet = CharSet.Auto)]
+    private static extern bool CredUnPackAuthenticationBuffer(int dwFlags,
+                                                               IntPtr pAuthBuffer,
+                                                               uint cbAuthBuffer,
+                                                               StringBuilder pszUserName,
+                                                               ref int pcchMaxUserName,
+                                                               StringBuilder pszDomainName,
+                                                               ref int pcchMaxDomainame,
+                                                               StringBuilder pszPassword,
+                                                               ref int pcchMaxPassword);
+
+    [DllImport("credui.dll", CharSet = CharSet.Auto)]
+    private static extern int CredUIPromptForWindowsCredentials(ref CREDUI_INFO notUsedHere,
+                                                                 int authError,
+                                                                 ref uint authPackage,
+                                                                 IntPtr InAuthBuffer,
+                                                                 uint InAuthBufferSize,
+                                                                 out IntPtr refOutAuthBuffer,
+                                                                 out uint refOutAuthBufferSize,
+                                                                 ref bool fSave,
+                                                                 int flags);
+
+
+
+    public static void GetCredentialsVistaAndUp(string serverName, out NetworkCredential networkCredential)
+    {
+        CREDUI_INFO credui = new CREDUI_INFO();
+        credui.pszCaptionText = "Please enter the credentails for " + serverName;
+        credui.pszMessageText = "DisplayedMessage";
+        credui.cbSize = Marshal.SizeOf(credui);
+        uint authPackage = 0;
+        IntPtr outCredBuffer = new IntPtr();
+        uint outCredSize;
+        bool save = false;
+        int result = CredUIPromptForWindowsCredentials(ref credui,
+                                                       0,
+                                                       ref authPackage,
+                                                       IntPtr.Zero,
+                                                       0,
+                                                       out outCredBuffer,
+                                                       out outCredSize,
+                                                       ref save,
+                                                       1 /* Generic */);
+
+        var usernameBuf = new StringBuilder(100);
+        var passwordBuf  = new StringBuilder(100);
+        var domainBuf = new StringBuilder(100);
+
+        int maxUserName = 100;
+        int maxDomain = 100;
+        int maxPassword = 100;
+        if (result == 0)
+        {
+            if (CredUnPackAuthenticationBuffer(0, outCredBuffer, outCredSize, usernameBuf, ref maxUserName,
+                                               domainBuf, ref maxDomain, passwordBuf, ref maxPassword))
+            {
+                //TODO: ms documentation says we should call this but i can't get it to work
+                //SecureZeroMem(outCredBuffer, outCredSize);
+
+                //clear the memory allocated by CredUIPromptForWindowsCredentials 
+                CoTaskMemFree(outCredBuffer);
+                networkCredential = new NetworkCredential()
+                                        {
+                                            UserName = usernameBuf.ToString(),
+                                            Password = passwordBuf.ToString(),
+                                            Domain = domainBuf.ToString()
+                                        };
+                return;
+            }
+        }
+
+        networkCredential = null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace bulkmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("ERROR need two args: template maillist.tsv");
+                Environment.Exit(1);
+            }
+
+            var template = File.ReadAllText(args[0]);
+            var maillist = ReadTsv(args[1]).ToList();
+
+            var service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+            NetworkCredential nc;
+            Credential.GetCredentialsVistaAndUp("office365", out nc);
+            service.Credentials = nc;
+            service.TraceEnabled = true;
+            service.TraceFlags = TraceFlags.All;
+            service.Url = new Uri("https://outlook.office365.com/ews/exchange.asmx");
+
+            Regex r = new Regex(@"^\s*([^:]+):\s*(.*)$");
+            foreach (var item in maillist)
+            {
+                var mail = new EmailMessage(service);
+
+                var s = template;
+                foreach (var kvp in item)
+                    s = s.Replace("[[" + kvp.Key + "]]", kvp.Value);
+
+                bool foundEmpty = false;
+                StringBuilder body = new StringBuilder();
+                foreach (var line in s.Split('\n'))
+                {
+                    if (foundEmpty) { body.AppendLine(line); continue; }
+                    if (string.IsNullOrWhiteSpace(line)) { foundEmpty = true; continue; }
+                    var m = r.Match(line);
+                    if (!m.Success) throw new Exception("bad header field " + line);
+                    switch (m.Groups[1].Value.ToLowerInvariant())
+                    {
+                        case "subject": mail.Subject = m.Groups[2].Value.Trim(); break;
+                        case "to": mail.ToRecipients.Add(m.Groups[2].Value.Trim()); break;
+                        case "cc": mail.CcRecipients.Add(m.Groups[2].Value.Trim()); break;
+                        case "bcc": mail.BccRecipients.Add(m.Groups[2].Value.Trim()); break;
+                        case "attach": mail.Attachments.AddFileAttachment(m.Groups[2].Value.Trim()); break;
+
+                        default:
+                            throw new Exception("unknown field in template: " + m.Groups[1].Value);
+                    }
+                }
+                mail.Body = body.ToString();
+
+                mail.SendAndSaveCopy();
+            }
+        }
+
+        public static IEnumerable<Dictionary<string, string>> ReadTsv(string filename)
+        {
+            var header = File.ReadLines(filename).First().Split('\t').ToArray();
+            foreach (var l in File.ReadLines(filename).Skip(1))
+            {
+                var d = new Dictionary<string, string>();
+                var v = l.Split('\t').ToArray();
+                for (int i = 0; i < header.Length; ++i)
+                    d[header[i]] = v[i];
+                yield return d;
+            }
+        }
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            Console.WriteLine("validating " + redirectionUrl);
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
